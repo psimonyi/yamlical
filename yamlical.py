cba992c91ce2dd801b40876b4be6a5383cc929eb
Peter Simonyi <pts@petersimonyi.ca>
2017-09-07 22:34:27 -0400

    Accept date format Monthname D, YYYY

--- yamlical.py
+++ yamlical.py
@@ -77,7 +77,7 @@ class YamlPluralString(str):
         return cls(node.value)
 
 TIME_FORMATS = ['h:mmA', 'h:mm A', 'hA', 'h A']
-DATE_FORMATS = ['dddd, MMMM D, YYYY', 'MMM D, YYYY']
+DATE_FORMATS = ['dddd, MMMM D, YYYY', 'MMM D, YYYY', 'MMMM D, YYYY']
 DT_FORMATS = ['YYYY-MM-DD h:mmA', 'YYYY-MM-DD h:mm']
 
 # TZID should really be data.calendar.timezone
